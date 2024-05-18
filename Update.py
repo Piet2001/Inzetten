4caa28bb7e9fe8a49a1eece5ed19c58801949a02
Piet2001 <piet2001@fireman.net>
2024-05-18 21:54:52 +0200

    Indexering voor volgorde behoud

--- Update.py
+++ Update.py
@@ -1,5 +1,4 @@
 import json
-import time
 
 print("start")
 
@@ -9,20 +8,23 @@ complete = open('complete.json')
 data_complete = json.load(complete)
 
 for m in data_live:
-    print(f"Checking mission {m['id']}")
+    print(f"?? Checking mission {m['id']}")
     mc = next((x for x in data_complete if x["id"] == m["id"]), None)
 
     if(mc == None):
-        print(f"{m['id']} doens't exist, will be added")
-        data_complete.append(m)
+        print(f"++ {m['id']} doens't exist, will be added")
+        index_live = data_live.index(m)
+        mission_live_prev = data_live[index_live - 1]
+        mission_complete_prev = next((x for x in data_complete if x["id"] == mission_live_prev["id"]))
+        index_complete_prev = data_complete.index(mission_complete_prev)
+        data_complete.insert(index_complete_prev + 1, m)
     else:
-        print(f"Comparing mission {m['id']}")
+        print(f"+- Comparing mission {m['id']}")
         if (m != mc):
-            print(f"Updating mission {m['id']}")
+            print(f"--> Updating mission {m['id']}")
             for z in data_complete:
                 if (z['id'] == m['id']):
                     index = data_complete.index(z)
-                    print(index)
                     data_complete[index] = m
 
 
